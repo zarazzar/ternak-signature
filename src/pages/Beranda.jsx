407bd96bfee1dba95c89baf3060940205a91dcf9
zarazzar <azzar.arza18@gmail.com>
2023-01-03 02:45:19 +0700

    updet

--- src/pages/Beranda.jsx
+++ src/pages/Beranda.jsx
@@ -4,7 +4,9 @@ export default class Beranda extends Component {
   render() {
     return (
       <div>
-        <h1>Beranda</h1>
+        <h1>Beranda
+        </h1>
+        <p> Halaman masih kosong karena masih dalam tahap desain </p>
       </div>
     )
   }
