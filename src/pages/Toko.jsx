45b2059fe13c6db4554e1cc326b5572b6635d0f2
zarazzar <azzar.arza18@gmail.com>
2023-01-04 15:17:56 +0700

    add stor

--- src/pages/Toko.jsx
+++ src/pages/Toko.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import Store from "../container/StorePost"
+
 
 export default class Toko extends Component {
   render() {
     return (
       <div>
         <h1>Toko</h1>
+        <Store/>
       </div>
     )
   }
